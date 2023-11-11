b16e3ff57839e77491f7b83969892c1175b0fd76
Gledson Lima <58941795+gledlima@users.noreply.github.com>
2023-11-11 14:31:38 -0300

    Update Resolucao.sql

--- Resolucao.sql
+++ Resolucao.sql
@@ -1,4 +1,4 @@
-select cliente.nome, casa.cor AS 'Cor da Casa', bairro.nome AS 'Nome do Bairro', carro.modelo from cliente
+select cliente.nome AS 'Cliente' ,  bairro.nome AS 'Bairro', casa.cor AS 'Cor da Casa', carro.modelo AS 'Modelo do Carro' from cliente
 join casa on casa.fk_cliente = cliente.id_cliente
-join bairro on fk_bairro = id_bairro
+join bairro on bairro.id_bairro = casa.fk_bairro
 join carro on carro.fk_cliente = cliente.id_cliente;
